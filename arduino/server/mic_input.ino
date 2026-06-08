4eab9bedd6baa9e74b5a05d3dfe715091a124faa
大菅海兎 <kaito0907usagi@gmail.com>
2026-06-08 17:26:15 +0900

    BPM生成アルゴリズムの修正

--- arduino/server/mic_input.ino
+++ arduino/server/mic_input.ino
@@ -5,16 +5,16 @@ void mic_setup(){
 bool mic_read(){
     bool judge=false;
     unsigned long start_t=millis();
-    if(last_time-start_t<250){//インターバルチェックH
+    if(start_t-last_time<250){//インターバルチェック
         return judge;
     }
     value=analogRead(MIC_PIN);
-    mic_window[win_id] = value;
-    win_id = (win_id + 1) % WINDOW_SIZE;
-    if (win_id == 0) {
-        win_full = true;
-    }
     if(!win_full){
+        mic_window[win_id] = value;
+        win_id = (win_id + 1) % WINDOW_SIZE;
+        if (win_id == 0) {
+            win_full = true;
+        }
         return 0;
     }
     float sum=0;
@@ -33,6 +33,14 @@ bool mic_read(){
     float threshold=mean+PEAK_SIGMA_FACTOR*sigma;
     if(value>threshold){
         judge = bpm_generate(start_t);
+        last_time=start_t;
+        return judge;//returnすることで閾値以上の値を閾値設定バッファに入れない
     }
+    mic_window[win_id] = value;
+    win_id = (win_id + 1) % WINDOW_SIZE;
+    if (win_id == 0) {
+        win_full = true;
+    }
+    last_time=start_t;
     return judge;
 }
