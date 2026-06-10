3b7fd1e8038d02ff8b9b2da2a716f77390874e80
kaito <kaito0907usagi@gmail.com>
2026-06-10 15:51:52 +0900

    BPM生成アルゴリズムの修正 (#9)
    * BPM生成アルゴリズムの修正
    
    * fix:拍手間隔の外れ値除去実装とアルゴリズム修正

--- arduino/server/bpm_manager.ino
+++ arduino/server/bpm_manager.ino
@@ -9,25 +9,56 @@ void bpm_setup(){
 
 bool bpm_generate(unsigned long last_t){
     //ウィンドウを埋める作業
-    if (clap_count < CLAP_MAX){
+    if(clap_count < CLAP_MAX){
         clap_times[clap_count] = last_t;
         clap_count++;
-    }else{
-        for (int i = 0; i < CLAP_MAX - 1; i++){
-            clap_times[i] = clap_times[i + 1];
+        // 5回分の拍手時刻が揃うまでBPMを計算しない
+        if(clap_count < CLAP_MAX){
+            return false;
         }
-        clap_times[CLAP_MAX - 1] = last_t;
+    }else{
+        for(int i=0;i<CLAP_MAX-1;i++){
+            clap_times[i] = clap_times[i+1];
+        }
+        clap_times[CLAP_MAX-1] = last_t;
     }
 
-    if (clap_count < CLAP_MAX){//ウィンドウの埋まり具合を検知
+    for(uint8_t i=0;i<CLAP_INTERVAL_COUNT;i++){//拍手間隔時間の計算と格納
+        clap_intervals[i] = clap_times[i+1]-clap_times[i];
+        clap_sorted_intervals[i] = clap_intervals[i];
+    }
+
+    for(uint8_t i=0;i<CLAP_INTERVAL_COUNT-1;i++){//拍手間隔時間を昇順に並び替え
+        for(uint8_t j=i+1;j<CLAP_INTERVAL_COUNT;j++){
+            if(clap_sorted_intervals[i]>clap_sorted_intervals[j]){
+                unsigned long temp = clap_sorted_intervals[i];
+                clap_sorted_intervals[i] = clap_sorted_intervals[j];
+                clap_sorted_intervals[j] = temp;
+            }
+        }
+    }
+
+    //中央値の計算（ウィンドウサイズ奇数のみ対応）
+    float median_interval =((float)clap_sorted_intervals[CLAP_INTERVAL_COUNT/2-1]+(float)clap_sorted_intervals[CLAP_INTERVAL_COUNT/2])/2.0;
+    //閾値の設定
+    float lower_threshold =median_interval*(1.0-INTERVAL_OUTLIER_RATIO);
+    float upper_threshold =median_interval*(1.0+INTERVAL_OUTLIER_RATIO);
+    float interval_sum = 0;
+    uint8_t valid_interval_count=0;
+
+    for(uint8_t i=0;i<CLAP_INTERVAL_COUNT;i++){
+        if(clap_intervals[i]>=lower_threshold && clap_intervals[i]<=upper_threshold){
+            interval_sum += clap_intervals[i];//ウィンドウの更新
+            valid_interval_count++;
+        }
+    }
+
+    // 有効な間隔が少ない場合はBPMを更新しない
+    if(valid_interval_count<2){
         return false;
     }
-    float interval_sum = 0;
-    for (uint8_t i = 0; i < CLAP_MAX - 1; i++){//過去ウィンドウの上書き
-        interval_sum += (clap_times[i + 1] - clap_times[i]);
-    }
 
-    float avg_interval_ms = interval_sum / (CLAP_MAX - 1);
+    float avg_interval_ms = interval_sum / valid_interval_count;
     float estimated_bpm = 60000 / avg_interval_ms;
 
     if (estimated_bpm < BPM_MIN){
